c9c61f45c040f16f99d54e1ccc483c46961f5bf3
vinithadevi-murugan <vinithadevi.murugan@syncfusion.com>
2017-07-14 16:47:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports={
+    positive:function(num){
+        if(num>0){
+            return num+ " Positive number";
+        }
+        else{
+            return num+ " Negative number";
+        }
+    }
+}
